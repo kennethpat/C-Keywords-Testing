8118d946121e2ac4ea21d1f3d5078dae3bd2a528
Kenneth Pat <59807013+kennethpat@users.noreply.github.com>
2021-03-27 16:48:31 -0700

    Create class.cpp

---
+++ class.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+using namespace std;
+
+class Rect
+{
+private:
+    int length;
+    int width;
+public:
+    Rect()
+    {
+        length = 0;
+        width = 0;
+    }
+    Rect(int l, int w)
+    {
+        length = l;
+        width = w;
+    }
+    int getLength()
+    {
+        return length;
+    }
+    int getWidth()
+    {
+        return width;
+    }
+    int getCircumference()
+    {
+        return (2 * (length + width));
+    }
+    int getArea()
+    {
+        return (length * width);
+    }
+};
+ 
+int main()
+{
+    Rect r = Rect(5, 2);
+    printf("The circumference is %d\n", r.getCircumference());
+    printf("The area is %d\n", r.getArea());
+    
+    return 0;
+}
