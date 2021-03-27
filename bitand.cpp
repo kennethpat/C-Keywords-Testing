8c8b8463a21de41f34da87e6cd77f20ba9242f6f
Kenneth Pat <59807013+kennethpat@users.noreply.github.com>
2021-03-27 15:43:01 -0700

    Create bitand.cpp

---
+++ bitand.cpp
@@ -0,0 +1,16 @@
+// "bitand" is used as an alternative for &
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+    bitset<4> a("1100");
+    bitset<4> b("0111");
+    bitset<4> c = a bitand b;
+    cout << "a is " << a << '\n';
+    cout << "b is " << b << '\n';
+    cout << "c is " << c << '\n';
+    
+    return 0;
+}
